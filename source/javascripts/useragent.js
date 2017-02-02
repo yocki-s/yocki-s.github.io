3f16951d7766ff7779ad2e6af42c4b53975cfe7b
Yocki <yocki.s@veritrans.co.id>
2017-02-02 11:18:24 +0700

    new page

--- source/javascripts/useragent.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	// DETECT USER AGENT
-    if (navigator.platform == "Win16" || navigator.platform == "Win32" || navigator.platform == "WinCE"){
-    	$("body").css("font-weight","400");
-    }else{
-    	$("body").css("font-weight","300");
-    }
-});
